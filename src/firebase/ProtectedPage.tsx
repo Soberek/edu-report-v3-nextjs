2eaf60fb104ee52a0ce8c5de10c460ee2d4335bc
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-17 18:29:10 +0200

    Refactor ProtectedPage to use useUser hook for authentication and improve loading state handling

--- src/firebase/ProtectedPage.tsx
+++ src/firebase/ProtectedPage.tsx
@@ -1,22 +1,51 @@
 "use client";
 
-import { useEffect } from "react";
+import { useUser } from "@/hooks/useUser";
+import { Box } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { onAuthStateChanged, getAuth } from "firebase/auth";
-import { app } from "@/firebase/config"; // Twoja konfiguracja
+import { useEffect } from "react";
 
 export default function ProtectedPage({ children }: { children: React.ReactNode }) {
+  const auth = useUser();
   const router = useRouter();
-  const auth = getAuth(app);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        router.replace("/login");
-      }
-    });
-    return () => unsubscribe();
-  }, [auth, router]);
+    if (!auth.loading && !auth.user) {
+      router.push("/login");
+    }
+  }, [auth.loading, auth.user, router]);
 
-  return <>{children}</>;
+  console.log("ProtectedPage loading:", auth.loading);
+  if (auth.loading) {
+    return (
+      <Box
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          background: "linear-gradient(135deg, #d400a6ff 0%, #da0000ff 100%)",
+        }}
+      >
+        <Box
+          style={{
+            padding: "2rem 3rem",
+            borderRadius: "1rem",
+            background: "#fff",
+            boxShadow: "0 4px 24px rgba(0,0,0,0.08)",
+            fontSize: "1.25rem",
+            fontWeight: 500,
+            color: "#2d3748",
+          }}
+        >
+          Ładuje się strona, poczekaj...
+        </Box>
+      </Box>
+    );
+  }
+
+  console.log("ProtectedPage user:", auth.user);
+
+  return auth.user ? children : null;
 }
