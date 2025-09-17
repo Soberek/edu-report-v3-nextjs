08f5efa7b4ecf9e12bdaad2a4e3cde3fc55cbeb6
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-17 17:46:19 +0200

    Add navRoutes constant for application navigation structure

---
+++ src/constants/nav-routes.ts
@@ -0,0 +1,50 @@
+export const navRoutes: Array<{
+  title: string;
+  path: string;
+  category: "main" | "database";
+}> = [
+  // Main routes
+  {
+    title: "Szkoły w programie",
+    path: "/",
+    category: "main",
+  },
+  {
+    title: "Harmonogram zadań edukacyjnych",
+    path: "schedule",
+    category: "main",
+  },
+  {
+    title: "Offline miernik budżetowy",
+    path: "offline-miernik-budzetowy",
+    category: "main",
+  },
+  {
+    title: "Spisy spraw",
+    path: "acts",
+    category: "database",
+  },
+
+  // Database routes
+  {
+    title: "Szkoły",
+    path: "schools",
+    category: "database",
+  },
+  {
+    title: "Programy",
+    path: "programs",
+    category: "database",
+  },
+  {
+    title: "Baza kontaktów",
+    path: "contacts",
+    category: "database",
+  },
+
+  // {
+  //   title: "Canva",
+  //   path: "canva",
+  //   category: 'main',
+  // },
+];
