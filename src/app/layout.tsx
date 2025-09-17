718de9b733800cb0c56835b4d2313d07b7107224
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-17 14:24:03 +0200

    Refactor layout component to enhance structure by adding providers and organizing imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import theme from "@/providers/ThemeProvider";
+import { ThemeProvider } from "@mui/material";
+import Navbar from "@/components/ui/top-navbar";
+import { NavProvider } from "@/providers/NavProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,10 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <NavProvider>
+              <Navbar />
+              {children}
+            </NavProvider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
