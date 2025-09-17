2a3e4fb64eb57145c10059dc8e983c6a399e9280
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-17 14:25:06 +0200

    Add WYKAZ_AKT constant for managing statistical and health program data

---
+++ src/constants/acts.ts
@@ -0,0 +1,183 @@
+export const WYKAZ_AKT = {
+  "0442": {
+    code: "0442",
+    name: "Sprawozdawczość statystyczna - Mierniki budżetowe",
+    description: "",
+    archiveCategory: "A", // kategoria archiwalna
+    subCategories: {},
+  },
+  "0444": {
+    code: "0444",
+    name: "Meldunki i raporty sytuacyjne - Ustawa tytoniowa",
+    description: "",
+    archiveCategory: "BE5", // kategoria archiwalna
+    subCategories: {},
+  },
+  "966": {
+    code: "966",
+    name: "Profilaktyka i promocja zdrowia",
+    description: "Program promujący aktywność fizyczną.",
+    archiveCategory: "BE10", // kategoria archiwalna
+    subCategories: {
+      "1": {
+        code: "966.1",
+        name: "Trzymaj formę",
+        description:
+          "Program mający na celu zapobieganie otyłości wśród dzieci.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "2": {
+        code: "966.2",
+        name: "HIV/AIDS",
+        description: "Program edukacyjny dotyczący HIV/AIDS.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "3": {
+        code: "966.3",
+        name: "Zdrowe zęby mamy, marchewkę zajadamy",
+        description: "Program promujący zdrowe nawyki żywieniowe.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "4": {
+        code: "966.4",
+        name: "Higiena naszą tarczą ochronną",
+        description: "Program promujący higienę osobistą.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "5": {
+        code: "966.5",
+        name: "Porozmawiajmy o zdrowiu i nowych zagrożeniach",
+        description: "Program edukacyjny na temat nowych zagrożeń zdrowotnych.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "6": {
+        code: "966.6",
+        name: "Profilaktyka używania napojów energetycznych i substancji psychoaktywnych",
+        description:
+          "Program mający na celu zapobieganie używaniu napojów energetycznych i substancji psychoaktywnych.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "7": {
+        code: "966.7",
+        name: "Promocja zdrowego stylu życia - inne",
+        description: "Program promujący zdrowy styl życia w różnych aspektach.",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "8": {
+        code: "966.8",
+        name: "Profilaktyka chorób zakaźnych i odzwierzęcych",
+        description:
+          "Program mający na celu zapobieganie chorobom zakaźnym i odzwierzęcym",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "9": {
+        code: "966.9",
+        name: "Profilaktyka chorób nowotworowych",
+        description:
+          "Program mający na celu zapobieganie chorobom nowotworowym",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "10": {
+        code: "966.10",
+        name: "Profilaktyka bezpiecznego grzybobrania i profilaktyka zatruć grzybami",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "11": {
+        code: "966.11",
+        name: "Promocja szczepień ochronnych",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "12": {
+        code: "966.12",
+        name: "Światowy Dzień Zdrowia",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "13": {
+        code: "966.13",
+        name: "Europejski Światowy Dzień Wiedzy o Antybiotykach",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "14": {
+        code: "966.14",
+        name: "Bezpieczeństwo dzieci podczas wypoczynku letniego i zimowego (bezpieczne ferie i wakacje)",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "15": {
+        code: "966.15",
+        name: "Seniorzy",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "16": {
+        code: "966.16",
+        name: "Promocja zdrowia psychicznego",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "17": {
+        code: "966.17",
+        name: "Wpływ czynników środowiskowych na zdrowie",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "18": {
+        code: "966.18",
+        name: "Profilaktyka wad postawy",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "19": {
+        code: "966.19",
+        name: "POWER UKRAINA",
+        description: "Program ",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+    },
+  },
+  "9011": {
+    code: "9011",
+    name: "Wymiana informacji między podmiotami w sprawach sanitarnych",
+    description: "",
+    archiveCategory: "BE10", // kategoria archiwalna
+    subCategories: {
+      "1": {
+        code: "9011.1",
+        name: "Wymiana informacji między podmiotami w sprawach sanitarnych \n Współpraca z Wojewódzką Stacją Sanitarno-Epidemiologiczną",
+        description: "Opis podkategorii 1",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+      "2": {
+        code: "9011.2",
+        name: "Wymiana informacji między podmiotami w sprawach sanitarnych \n Współpraca z innymi jednostkami",
+        description: "Opis podkategorii 2",
+        archiveCategory: "BE10",
+        subCategories: {},
+      },
+    },
+  },
+} as const;
