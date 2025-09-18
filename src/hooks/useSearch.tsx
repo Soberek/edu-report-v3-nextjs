8617a66d7a5a34dcd2d151b1366ff0cf7a805b3f
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-18 17:37:34 +0200

    fix: Clean up formatting in useSearch hook and ensure "use client" directive is included

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { createContext, useContext } from "react";
 
 type SearchContextType = {
@@ -5,9 +6,7 @@ type SearchContextType = {
   setSearchTerm: (term: string) => void;
 };
 
-export const SearchContext = createContext<SearchContextType | undefined>(
-  undefined,
-);
+export const SearchContext = createContext<SearchContextType | undefined>(undefined);
 
 export const useSearch = () => {
   return useContext(SearchContext);
