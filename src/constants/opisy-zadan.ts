55ebc133f7b8b0a8e4519d34b6418113d1130a7d
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-22 13:30:57 +0200

    feat: Add educational activities descriptions with icons to OPISY_ZADAN constant

---
+++ src/constants/opisy-zadan.ts
@@ -0,0 +1,26 @@
+export const OPISY_ZADAN = {
+  " Bezpieczne Wakacje - Prelekcja": {
+    icon: "🛡️",
+    opis: `W ramach akcji „Bezpieczne wakacje” przeprowadzono prelekcje edukacyjne z uczniami klas I-VIII. Podczas spotkań omówiono zasady bezpiecznego spędzania czasu wolnego, zachowania nad wodą, bezpieczeństwo w ruchu drogowym oraz zagrożenia związane z używaniem substancji psychoaktywnych w okresie wakacyjnym. Prelekcje zrealizowano we współpracy z funkcjonariuszem policji.`,
+  },
+  "Bezpieczne Wakacje - Stoisko": {
+    icon: "🏖️",
+    opis: ``,
+  },
+  HNT: {
+    icon: "🧼",
+    opis: `W ramach realizacji programu "Higiena naszą tarczą ochronną" przeprowadzono zajęcia edukacyjne z uczniami klas I. Podczas tych zajęć rozmawialiśmy o znaczeniu higieny dla naszego zdrowia. Omówiliśmy również, gdzie najczęściej znajdują się bakterie i jak mogą one wpływać na nasze zdrowie. Dzieci dowiedziały się, jakie proste codzienne nawyki mogą pomóc w utrzymaniu czystości oraz jak dbać o higienę osobistą. Dzieci poznały najlepsze praktyki związane z myciem rąk, dbaniem o higienę jamy ustnej oraz utrzymaniem czystości. Poprzez interaktywne zajęcia i dyskusje dzieci zdobyły praktyczną wiedzę na temat higieny, która pomoże im w codziennym życiu, zadba o ich zdrowie oraz zapobiegnie przenoszeniu chorób zakaźnych.`,
+  },
+  "Bezpieczne Ferie": {
+    icon: "⛷️",
+    opis: `Celem zadania jest edukacja uczniów na temat zasad bezpieczeństwa podczas ferii zimowych, w tym bezpiecznego korzystania z atrakcji zimowych, takich jak narty, snowboard czy łyżwy. Zadanie ma na celu zwiększenie świadomości dzieci i młodzieży na temat potencjalnych zagrożeń związanych z aktywnościami zimowymi oraz nauczenie ich, jak unikać niebezpiecznych sytuacji, aby zapewnić im bezpieczne i przyjemne ferie.`,
+  },
+  POZ: {
+    icon: "🎬",
+    opis: `W ramach realizacji programu „Porozmawiajmy o zdrowiu i nowych zagrożeniach” przeprowadzono zajęcia edukacyjne z uczniami klasy VII. W trakcie zajęć wykorzystano filmy edukacyjne. Omówiono skutki zdrowotne wynikające z używania substancji psychoaktywnych, w tym nowych narkotyków, alkoholu, e-papierosów i napojów energetycznych.  Zajęcia przeprowadzono z policją.`,
+  },
+  Zdrowe: {
+    icon: "🍏",
+    opis: `Przeprowadzono prelekcje dla uczniów przedszkolnych w wieku 5–6 lat, które miały na celu zwiększenie ich świadomości na temat prawidłowej higieny jamy ustnej oraz znaczenia zdrowego stylu życia. W trakcie zajęć wykorzystano prezentację multimedialną, która w przystępny sposób przedstawiła podstawowe zasady higieny i zdrowego odżywiania. Dzieci aktywnie uczestniczyły w interaktywnym quizie, który pomógł im utrwalić zdobytą wiedzę w formie zabawy. Dodatkowo przeprowadzono praktyczne warsztaty z mycia zębów, podczas których najmłodsi mieli okazję ćwiczyć prawidłową technikę szczotkowania na modelu sztucznej szczęki. Uzupełnieniem zajęć było omówienie zasad zdrowego odżywiania z wykorzystaniem modelu „Talerza Zdrowych Posiłków”, który pomógł dzieciom lepiej zrozumieć, jakie produkty powinny znaleźć się w codziennej diecie, aby wspierać zdrowie zębów i całego organizmu.`,
+  },
+};
