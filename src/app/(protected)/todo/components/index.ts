8bdee19a4d8d1fe1ed25fb016770d187d260d348
Sober <59960384+Soberek@users.noreply.github.com>
2025-09-30 19:00:07 +0200

    feat: add components index file for todo module exports

---
+++ src/app/(protected)/todo/components/index.ts
@@ -0,0 +1,6 @@
+export { TodoForm } from "./TodoForm";
+export { TodoControls } from "./TodoControls";
+export { TodoItem } from "./TodoItem";
+export { TodoCalendar } from "./TodoCalendar";
+export { TodoEditDialog } from "./TodoEditDialog";
+export { TodoKanban } from "./TodoKanban";
