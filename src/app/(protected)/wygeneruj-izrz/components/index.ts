312f300cec32e743a7da20feaf3621ca2436edbe
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-04 17:53:44 +0200

    feat(index): re-export TaskDescriptionTemplateSelector and AudienceGroupBuilder components

--- src/app/(protected)/wygeneruj-izrz/components/index.ts
+++ src/app/(protected)/wygeneruj-izrz/components/index.ts
@@ -1,6 +1,8 @@
 // Component exports
 export { TemplateSelector } from "./TemplateSelector";
 export { FormField } from "./FormField";
+export { TaskDescriptionTemplateSelector } from "./ViewerDescriptionTemplateSelector";
+export { AudienceGroupBuilder } from "./AudienceGroupBuilder";
 
 // Re-export types for convenience
 export type { TemplateSelectorProps, FormFieldProps } from "../types";
