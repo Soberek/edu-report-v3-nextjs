f213afa119e5904ef238bb7fb0af7ba5912c1889
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-04 20:31:04 +0200

    Refactor: re-export components, constants, hooks, types, and utils in auth index

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,2 +1,7 @@
 export { RoleProtected } from "./RoleProtected";
 export { AdminOnly } from "./AdminOnly";
+export * from "./components";
+export * from "./constants";
+export * from "./hooks";
+export * from "./types";
+export * from "./utils";
