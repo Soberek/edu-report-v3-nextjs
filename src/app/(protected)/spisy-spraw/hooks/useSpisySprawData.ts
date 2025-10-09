7e07b5ee23cb4030de7e6b9dca72ce2893502495
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-09 09:19:03 +0200

    Refactor sorting logic in useSpisySprawData to handle specific code cases and improve readability

--- src/app/(protected)/spisy-spraw/hooks/useSpisySprawData.ts
+++ src/app/(protected)/spisy-spraw/hooks/useSpisySprawData.ts
@@ -4,16 +4,23 @@ import { createActsOptions } from "../constants";
 import type { SpisySprawState } from "../types";
 
 export const useSpisySprawData = (state: SpisySprawState) => {
-  const { actRecords, actRecordsError, actRecordsLoading } = useAct();
+  const { actRecords, actRecordsError, actRecordsLoading, refetchActRecords } = useAct();
 
   const actsOptions = useMemo(() => createActsOptions(), []);
 
   const actsOptionsCodes = useMemo(() => actsOptions.map((option) => option.code), [actsOptions]);
 
   const sortedCaseRecords = useMemo(() => {
-    return actRecords
-      .filter((record) => record.code === state.selectedCode.code)
-      .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    let filtered;
+    if (state.selectedCode.code === "966") {
+      filtered = actRecords.filter((record) => record.referenceNumber.startsWith("OZiPZ.966"));
+    } else if (state.selectedCode.code) {
+      filtered = actRecords.filter((record) => record.code === state.selectedCode.code);
+    } else {
+      filtered = actRecords;
+    }
+    const sorted = [...filtered].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    return sorted;
   }, [actRecords, state.selectedCode.code]);
 
   const getErrorMessages = () => {
