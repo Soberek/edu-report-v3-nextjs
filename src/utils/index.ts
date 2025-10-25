99a9c51f968d97981e77119d515af4ed992da823
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-25 17:35:13 +0200

    refactor: update utils index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,6 @@ export * from "./formUtils";
 export * from "./dateUtils";
 export * from "./validationUtils";
 export * from "./apiUtils";
+export * from "./queryErrorHandler";
+export * from "./errorLogger";
+export * from "./globalErrorHandler";
